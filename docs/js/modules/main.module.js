cd73afe31dc73749cf547b1c29635c8959b0df99
Oreberry <aubreymonk1@gmail.com>
2020-02-12 19:24:47 +0000

    Added broker params and resolve to main module

--- docs/js/modules/main.module.js
+++ docs/js/modules/main.module.js
@@ -7,9 +7,41 @@
       $stateProvider
         .state('events_list', {
           cache: false,
-          url: '/events_list',
+          url: '/index?uuid&brokerHost&brokerPort&username&password&ssl',
+          params: {
+              uuid : {
+                  dynamic: false
+              },
+              brokerHost: {
+                  dynamic: false
+              },
+              brokerPort: {
+                 dynamic: false
+              },
+              username: {
+                  dynamic : false
+              },
+              password: {
+                  dynamic : false
+              },
+              ssl:{
+                  dynamic : false
+              }
+          },
           templateUrl: 'js/states/event-list/events.list.html',
-          controller: 'eventsListCtrl as vm'
+          controller: 'eventsListCtrl as vm',
+          resolve: {
+            broker: ['$stateParams','brokerDetails', function ($stateParams,brokerDetails) {
+            
+                if($stateParams.uuid) brokerDetails.UUID = $stateParams.uuid;
+                if($stateParams.brokerHost) brokerDetails.HOST = $stateParams.brokerHost;
+                if($stateParams.brokerPort) brokerDetails.PORT = $stateParams.brokerPort;
+                if($stateParams.username) brokerDetails.USERNAME = $stateParams.username;
+                if($stateParams.password) brokerDetails.PASSWORD = $stateParams.password;
+                if($stateParams.ssl) brokerDetails.SSL = ($stateParams.ssl.toLowerCase() == 'true');
+            }]
+        }
+          
         })
         .state('events_update', {
           cache: false,
